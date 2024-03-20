2b3546fdbcda5667fdca8411f71bfa44a3cb18ed
GigiLi89 <li.gigi89@gmail.com>
2024-03-20 12:24:09 +0000

    added and changed javascript

--- assets/script.js
+++ assets/script.js
@@ -2,116 +2,97 @@ const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
+
 //To store index and score//
 let  currentQuestionIndex = 0;
-var score = 0;
+let score = 0;
 
 // Questions and answer options (questions taken from beano and bbc//
 const questions = [
     {
         question: "What was the name of Wendy's dog in Peter Pan?",
         answers: [
-            {text: "Nana", correct: true},
-            {text: "Bobo", correct: false},
-            {text: "Lulu", correct: false},
-            {text: "Jojo", correct: false},
+            {text: "Nana", correct: true}, {text: "Bobo", correct: false}, 
+            {text: "Lulu", correct: false}, {text: "Jojo", correct: false},
         ]
     },
     {
         question: "Who was Bambi's rabbit friend?",
         answers: [
-            {text: "Peter", correct: false},
-            {text: "Thumper", correct: true},
-            {text: "Drumper", correct: false},
-            {text: "Grumper", correct: false},
+            {text: "Peter", correct: false}, {text: "Thumper", correct: true},
+            {text: "Drumper", correct: false}, {text: "Grumper", correct: false},
         ]  
     },
     {
         question: "What is the name of Simba's dad in The Lion King?",
         answers: [
-            {text: "Timon", correct: false},
-            {text: "Pumba", correct: false},
-            {text: "Nala", correct: false},
-            {text: "Mufasa", correct: true},
+            {text: "Timon", correct: false}, {text: "Pumba", correct: false},
+            {text: "Nala", correct: false}, {text: "Mufasa", correct: true},
         ]  
     },
     {
         question: "In Monsters Inc, what do the monsters collect in order to power the city?",
         answers: [
-            {text: "Recycled paper", correct: false},
-            {text: "Children's screams", correct: true},
-            {text: "Coal", correct: false},
-            {text: "Nuclear stuff", correct: false},
+            {text: "Recycled paper", correct: false}, {text: "Children's screams", correct: true},
+            {text: "Coal", correct: false}, {text: "Nuclear stuff", correct: false},
         ]  
     },
     {
         question: "Who sang the song Under The Sea in The Little Mermaid?",
         answers: [
-            {text: "Sebastian", correct: true},
-            {text: "Dory", correct: false},
-            {text: "Nemo", correct: false},
-            {text: "Flounder", correct: false},
+            {text: "Sebastian", correct: true}, {text: "Dory", correct: false},
+            {text: "Nemo", correct: false}, {text: "Flounder", correct: false},
         ]  
     },
     {
         question: "Who was Snow White's enemy?",
         answers: [
-            {text: "Maleficent", correct: false},
-            {text: "The Evil Queen", correct: true},
-            {text: "Cruella De Vil", correct: false},
-            {text: "Scar", correct: false},
+            {text: "Maleficent", correct: false}, {text: "The Evil Queen", correct: true},
+            {text: "Cruella De Vil", correct: false}, {text: "Scar", correct: false},
         ]  
     },
     {
         question: "How many Toy Story films are there in total?",
         answers: [
-            {text: "2", correct: false},
-            {text: "5", correct: false},
-            {text: "3", correct: false},
-            {text: "4", correct: true},
+            {text: "2", correct: false}, {text: "5", correct: false},
+            {text: "3", correct: false}, {text: "4", correct: true},
         ]  
     },
     {
         question: "Which film was named after Walt Disney?",
         answers: [
-            {text: "Robin Hood", correct: false},
-            {text: "Peter Pan", correct: false},
-            {text: "Wall-E", correct: true},
-            {text: "Pinnochio", correct: false},
+            {text: "Robin Hood", correct: false}, {text: "Peter Pan", correct: false},
+            {text: "Wall-E", correct: true}, {text: "Pinnochio", correct: false},
         ]  
     },
     {
         question: "What is the name of Donald Duck's sister?",
         answers: [
-            {text: "Dumbella", correct: true},
-            {text: "Dumbo", correct: false},
-            {text: "Daisy", correct: false},
-            {text: "Della", correct: false},
+            {text: "Dumbella", correct: true}, {text: "Dumbo", correct: false},
+            {text: "Daisy", correct: false}, {text: "Della", correct: false},
         ]  
     },
     {
         question: "What is the name of the snowman in Frozen?",
         answers: [
-            {text: "Oleg", correct: false},
-            {text: "Olaf", correct: true},
-            {text: "Olov", correct: false},
-            {text: "Harry", correct: false},
+            {text: "Oleg", correct: false}, {text: "Olaf", correct: true},
+            {text: "Olov", correct: false}, {text: "Harry", correct: false},
         ]  
     },
 ];
 
 //Start quiz//
-function startQuiz(){
+function startQuiz() {
     currentQuestion = 0;
     nextButton.innerHTML ="Next";
     showQuestion();
 }
 
 //Display question//
-function showQuestion(){
+function showQuestion() {
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
-    questionElement.innerHTML = currentQuestion.question;
+    questionElement.textContent = currentQuestion.question;
 
 
     // --------------------------------------------------------//
@@ -119,24 +100,29 @@ function showQuestion(){
 
 //Display answer options// Hela koden från YT Tutorial
 currentQuestion.answers.forEach(answer => {
-    const button = document.createElement("button");
-    button.innerHTML = answer.text;
-    button.classList.add("btn");
+    const button = createButton(answer.text, answer.correct);
     answerButtons.appendChild(button);
-    nextButton.style.display = "block";
-    if(answer.correct){
-        button.dataset.correct = answer.correct;
-    }
     button.addEventListener("click", selectAnswer);
 });
 }
 
-//Removes previous content// Tagit hela koden fron YT tutorial
-function resetState(){
-    while(answerButtons.firstChild){
-        answerButtons.removeChild(answerButtons.firstChild);
+function createButton(text, correct) {
+    const button = document.createElement("button");
+    button.textContent = text;
+    button.classList.add("btn");
+    if (correct) {
+        button.dataset.correct = correct;
     }
+    return button;
 }
+
+//Removes previous content// Tagit hela koden fron YT tutorial
+function resetState() {
+    answerButtons.innerHTML = "";
+    nextButton.style.display = "block";
+}
+
+let userAnswered = false;
 
 //Check answer//
 function selectAnswer(e){
@@ -159,6 +145,7 @@ button.disabled = true;
 nextButton.style.display = "block";
 }
 
+
 //If there are no more questions it will display score//
 function handleNextButton(){
     currentQuestionIndex++;
@@ -171,8 +158,9 @@ function handleNextButton(){
 
 function showScore(){
     resetState();
-    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
-    nextButton.innerHTML = "Play Again";
+    questionElement.textContent = `You scored ${score} out of ${questions.length}!`;
+    nextButton.textContent = "Play Again";
+    currentQuestionIndex = 0;
 }
 
 nextButton.addEventListener("click", () => {
